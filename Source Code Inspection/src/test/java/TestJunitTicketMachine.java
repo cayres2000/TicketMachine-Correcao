5045b32d1fe98cb00f69ff0a7f8bbcdf9f8c86db
Vinicius Cayres <56272453+cayres2000@users.noreply.github.com>
2023-10-28 15:54:44 -0300

    Update TestJunitTicketMachine.java

--- Source Code Inspection/src/test/java/TestJunitTicketMachine.java
+++ Source Code Inspection/src/test/java/TestJunitTicketMachine.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
-public class TestTicketMachine {
+public class TestJunitTicketMachine {
 
     private TicketMachine ticketMachine;
 
