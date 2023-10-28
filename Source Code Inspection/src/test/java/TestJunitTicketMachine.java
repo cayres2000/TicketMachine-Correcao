f872ad92d0d7e7fc05ba6303aa55792647036b7e
Jhonnathan de Sousa Pessoa <91705066+JhonnathanL@users.noreply.github.com>
2023-10-28 18:11:28 -0300

    Update TestJunitTicketMachine.java

--- Source Code Inspection/src/test/java/TestJunitTicketMachine.java
+++ Source Code Inspection/src/test/java/TestJunitTicketMachine.java
@@ -22,4 +22,30 @@ public class TestJunitTicketMachine {
         assertEquals(5,ticketTroco.papeisMoeda[1].getValor());   
         assertEquals(2,ticketTroco.papeisMoeda[0].getValor());
     }
+
+    @Test
+    public void TesteClasseTrocoQuantidade() 
+    {
+        Troco ticketTroco = new Troco(189);
+        
+        assertEquals(100,ticketTroco.papeisMoeda[5].getQuantidade());     
+        assertEquals(50,ticketTroco.papeisMoeda[4].getQuantidade());       
+        assertEquals(20,ticketTroco.papeisMoeda[3].getQuantidade());
+        assertEquals(10,ticketTroco.papeisMoeda[2].getQuantidade());
+        assertEquals(5,ticketTroco.papeisMoeda[1].getQuantidade());   
+        assertEquals(2,ticketTroco.papeisMoeda[0].getQuantidade());
+    }
+
+    @Test
+    public void TesteClasseTicketMachine() 
+    {
+        TicketMachine ticketMachine = new TicketMachine(10);
+        ticketMachine.inserir(20);
+        
+        String result = "*****************\n";
+        result += "*** R$ 20,00 ****\n";//Corrigido
+        result += "*****************\n";
+
+        assertEquals(result,ticketMachine.imprimir()));
+    }
 }
